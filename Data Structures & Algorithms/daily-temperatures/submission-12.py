ad419fe87969e7685f00d15f4aaa0cbfb78dec72
unixisking-grind <brahimi.sdhmd@outlook.com>
2026-06-07 14:42:42 +0200

    Add: daily-temperatures - submission-12

---
+++ Data Structures & Algorithms/daily-temperatures/submission-12.py
@@ -0,0 +1,21 @@
+class Solution:
+    # Monotonic decreasing Stack
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0] * len(temperatures)
+        stack = []
+
+        for idx, temp in enumerate(temperatures):
+            if not stack or temp < temperatures[stack[-1]]:
+                stack.append(idx)
+            else:
+                while stack and temperatures[stack[-1]] < temp:
+                    resIdx = stack.pop()
+                    result[resIdx] = idx - resIdx
+
+
+                stack.append(idx)
+        return result
+
+
+        
+
